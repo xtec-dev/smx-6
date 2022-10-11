2b9259965d80128c0f97cbd4c77f7f9c387e865c
David de Mingo <david@optersoft.com>
2022-10-11 23:16:37 +0000

    add azure.sh

---
+++ uf2/azure/azure.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+case $1 in
+    install)
+        if [ ! -f /usr/bin/az ]; then
+            curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+        fi
+
+        if [ ! -f /usr/bin/azcopy ]; then
+
+            wget https://aka.ms/downloadazcopy-v10-linux
+            tar -xvf downloadazcopy-v10-linux
+            
+            chmod +x ./azcopy_linux_amd64_*/azcopy
+            sudo mv ./azcopy_linux_amd64_*/azcopy /usr/bin/azcopy
+            
+            rm -f downloadazcopy-v10-linux
+            rm -rf ./azcopy_linux_amd64_*/
+
+        fi
+    ;;
+    
+    login)
+        az login --tenant learn.docs.microsoft.com
+        az configure --defaults group=`az group list --query '[].name' --output tsv`
+    ;;
+
+    *)
+        echo "unkown command"
+    ;;
+
+esac
