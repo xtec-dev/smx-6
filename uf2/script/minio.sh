8fdfb73d9a14e24b7cad549442ae5836f3b189e3
David de Mingo <david@optersoft.com>
2022-10-11 22:28:48 +0000

    minio backup works

--- uf2/script/minio.sh
+++ uf2/script/minio.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 file="data.tar.gz"
+password="password"
 
 case $1 in 
+    
     backup)
         tar czf $file data
-        openssl enc -aes-256-cbc -pbkdf2 -k password -in $file -out $file
-#mc cp $file local/data
-        #rm $file
+        openssl enc -aes-256-cbc -pbkdf2 -k $password -in $file -out $file
+        
+        mc mb -p local/data
+        mc cp $file local/data
+        
+        rm $file
         ;;
+
     restore)
         ;;
+    
     *)
         echo "minio backup | restore"
     ;;
 
+esac
