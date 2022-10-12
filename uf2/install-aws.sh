7965af0f61dc2268abe6e6b8f6dcdcf7b02ea274
David de Mingo <david@optersoft.com>
2022-10-12 12:28:26 +0000

    set aws cli output json

--- uf2/install-aws.sh
+++ uf2/install-aws.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+KEY="3BAA2298F3AAB38625D5"
+SECRET="hzZPb21La2YiERrT0qvcpuJR6m1mMaRd9dx66qrB"
+
+AWS_PATH=$HOME/.aws
+
 
 if [ ! -f /usr/local/bin/aws ]; then
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
@@ -10,12 +15,19 @@ if [ ! -f /usr/local/bin/aws ]; then
 fi
 
 # keys on https://console.filebase.com/keys
-#aws configure
 
-mkdir -p ~./aws
+# configure
+
+mkdir -p $AWS_PATH
+
+echo "[default]
+aws_access_key_id =  ${KEY} 
+aws_secret_access_key = ${SECRET}
+" > $AWS_PATH/credentials
+
 echo "[default]
 region = us-east-1
-output = Optional
-" >> ~./aws/config
+output = json
+" > $AWS_PATH/config
 
 # aws --endpoint https://s3.filebase.com s3 ls
