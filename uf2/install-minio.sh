76cd0ac1a02e8fcf627b73a33aa04673842c07d3
David de Mingo <david@optersoft.com>
2022-10-14 09:34:41 +0000

    minio works

--- uf2/install-minio.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -f /usr/local/bin/mc ]; then
-    wget https://dl.min.io/client/mc/release/linux-amd64/mc
-    chmod +x mc
-    sudo mv mc /usr/local/bin
-fi
-
-mc alias set local http://127.0.0.1:9000 admin password
