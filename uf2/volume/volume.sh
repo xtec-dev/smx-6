7e0aba80c24cfd685fb122dad7e9d1acd37890d3
David de Mingo <david@optersoft.com>
2022-10-17 20:17:59 +0200

    Add volume sh

---
+++ uf2/volume/volume.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+[ $# -ne 1 ] && echo "Usage: $0 <volume>" 1>&2 && exit 1
+
+docker run --rm -it \
+    --mount type=bind,src=${PWD},dst=/host \
+    --volume $1:/volume \
+    busybox:1.35
+
+# docker run --rm -it /bin/bash amazon/aws-cli:2.8.3 ls
