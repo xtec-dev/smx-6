f9ad9d19d3f7825efe4c29c782189f7514170ca2
David de Mingo <david@optersoft.com>
2022-10-21 09:09:52 +0000

    fix minor change on script

--- tmp/script.sh
+++ tmp/script.sh
@@ -8,6 +8,6 @@ wget -q https://ca.wikipedia.org/wiki/Girona -P david
 wget -q https://ca.wikipedia.org/wiki/Barcelona -P david
 wget -q https://ca.wikipedia.org/wiki/Tarragona -P david
 
-tar cf david.tar david
 
-rm -rf david
+
+
