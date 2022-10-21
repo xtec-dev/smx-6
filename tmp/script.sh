0bdba69da7c7bb6f28b01873f2a094713a71c421
David de Mingo <david@optersoft.com>
2022-10-21 09:07:58 +0000

    moved script.sh to tmp

---
+++ tmp/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+shopt -s extglob
+rm -rf * !("script.sh")
+
+mkdir david
+wget -q https://ca.wikipedia.org/wiki/Girona -P david
+wget -q https://ca.wikipedia.org/wiki/Barcelona -P david
+wget -q https://ca.wikipedia.org/wiki/Tarragona -P david
+
+tar cf david.tar david
+
+rm -rf david
