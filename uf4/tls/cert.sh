90aa175a6a75ee569bc3ae7751e0dd13b7079cd0
David de Mingo <david@optersoft.com>
2022-11-12 21:24:15 +0100

    uf4 nginx.sh finished

--- uf4/tls/cert.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p certs
-
-openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout xtec.key -out xtec.crt
--- uf4/tls/nginx.sh
+++ uf4/tls/nginx.sh
@@ -3,19 +3,27 @@
 cat >nginx.conf <<EOF
 server {
   listen 80;
+  server_name localhost;
+  # production server: return 301 https://$host$request_uri;
+  return 302 https://localhost$request_uri;
+}
+server {
   listen 443 ssl http2;
   server_name localhost;
   location / {
     root /usr/share/nginx/html;
     index index.html index.htm;
   }
+  ssl_certificate /usr/share/nginx/certs/xtec.crt;
+  ssl_certificate_key /usr/share/nginx/certs/xtec.key;
   ssl_protocols TLSv1.2 TLSv1.3;
   ssl_prefer_server_ciphers on;
   ssl_ciphers ECDH+AESGCM:ECDH+AES256-CBC:ECDH+AES128-CBC:DH+3DES:!ADH:!AECDH:!MD5;
 }
 EOF
 
-mkdir -p log
+mkdir -p certs
+! test -f certs/xtec.key && openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout certs/xtec.key -out certs/xtec.crt
 
 mkdir -p www
 ! test -f www/index.html && cat >www/index.html <<EOF
@@ -23,11 +31,9 @@ mkdir -p www
 EOF
 
 docker run --rm \
-    --mount type=bind,src=${PWD}/nginx.conf,dst=/etc/nginx/conf.d/default.conf,readonly=true \
-    --mount type=bind,src=${PWD}/www,dst=/usr/share/nginx/html/,readonly=true \
-    --publish 8080:80 \
-    --publish 8443:443 \
-    nginx:1.23
-
-echo "Listening on http://localhost:8080"
-echo "Listening on https://localhost:8443"
+  --mount type=bind,src=${PWD}/nginx.conf,dst=/etc/nginx/conf.d/default.conf,readonly=true \
+  --mount type=bind,src=${PWD}/certs,dst=/usr/share/nginx/certs/,readonly=true \
+  --mount type=bind,src=${PWD}/www,dst=/usr/share/nginx/html/,readonly=true \
+  --publish 80:80 \
+  --publish 443:443 \
+  nginx:1.23
